083fa206903047da30730ee13ec334c0ee40f64f
Mahmoud Nabil <36068951+EMahmoudNabil@users.noreply.github.com>
2024-12-02 23:31:22 +0200

    edit3

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,35 +1,37 @@
-  import Container from "@/components/Container";
-  import { getSingleProudct, getTrendingProducts } from "@/helpers";
-  import { Products } from "../../type";
-  import ProductsData from "@/components/ProductsData";
-  import SignleProduct from "@/components/SignleProduct";
+import Container from "@/components/Container";
+import { getSingleProudct } from "@/helpers";
+import { Products } from "../../type";
+import ProductsData from "@/components/ProductsData";
+import SignleProduct from "@/components/SignleProduct";
 import { productData } from "@/constants/data";
-   
-  type Props = {
-    searchParams: { [key: string]: string | string[] | undefined };
-  };
 
-  const ProuctPage = async ({ searchParams }: Props) => {
-    const _idString = searchParams?._id;
-    const _id = Number(_idString);
-    const product = getSingleProudct(_id);
-    // const data = await getTrendingProducts();
+type Props = {
+  searchParams: { [key: string]: string | string[] | undefined };
+};
 
-    return (
-      <div>
-        <Container>
-          <SignleProduct product={product} />
-          <div>
-            <p className="text-xl py-1 font-semibold">Tranding Products</p>
-            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4">
-              {productData?.map((item: Products) => (
-                <ProductsData key={item._id} item={item} />
-              ))}
-            </div>
+const ProuctPage = async ({ searchParams }: Props) => {
+  const _idString = searchParams?._id;
+  const _id = Number(_idString);
+  const product = getSingleProudct(_id);
+
+  // تحديد عرض 4 منتجات فقط من المنتجات الرائجة
+  const trendingProducts = productData.slice(0, 4); // أخذ أول 4 منتجات فقط
+
+  return (
+    <div>
+      <Container>
+        <SignleProduct product={product} />
+        <div>
+          <p className="text-xl py-1 font-semibold">Tranding Products</p>
+          <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4">
+            {trendingProducts?.map((item: Products) => (
+              <ProductsData key={item._id} item={item} />
+            ))}
           </div>
-        </Container>
-      </div>
-    );
-  };
+        </div>
+      </Container>
+    </div>
+  );
+};
 
-  export default ProuctPage;
+export default ProuctPage;
